dad03e78069d5407797032820646fd875eefb0dc
Anmol Garg <anmolgarg240@gmail.com>
2023-11-28 05:58:02 +0530

    Navbar added

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,61 @@
-import './App.css';
-import React from 'react';
-import Home from './Home';
+import React from "react";
+import Navbar from "./components/navbar";
+import { Routes, Route } from "react-router-dom";
 
-function App() {
+const Home = () => {
   return (
-    <div>
-      <Home />
-    </div>
+    <>
+      <Navbar />
+      <section className="hero-section">
+      <p>Home</p>
+      </section>
+    </>
   );
-}
+};
 
+const About = () => {
+  return (
+    <>
+      <Navbar />
+      <section className="hero-section">
+        <h1>The Business Of Lending Is Transforming.</h1>
+        <p>Are your Collections capabilities keeping up?</p>
+      </section>
+    </>
+  );
+};
+
+const Service = () => {
+  return (
+    <>
+      <Navbar />
+      <section className="hero-section">
+      <p>Sevices</p>
+      </section>
+    </>
+  );
+};
+
+const Contact = () => {
+  return (
+    <>
+      <Navbar />
+      <section className="hero-section">
+      <p>Contact Us</p>
+      </section>
+    </>
+  );
+};
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/service" element={<Service />} />
+      <Route path="/contact" element={<Contact />} />
+    </Routes>
+  );
+};
 
 export default App;
