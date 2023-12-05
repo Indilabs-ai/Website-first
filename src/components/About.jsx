df8fa38264af85f0058a28b30b8a95fb46e83c40
Anmol Garg <anmolgarg240@gmail.com>
2023-12-05 05:36:44 +0530

    made-Navbar-Home-Responsive

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,26 +7,23 @@ const About = () => {
             <div className='about-container'>
                 <h1 className='about-heading'>About Our Product</h1>
                 <div className='about-para-container'>
-                    <p className='about-para'>Simply execute this code in your JavaScript file or script, and it will remove all stored data in the local storage for the current domain.</p>
+                    <p className='about-para'>Our solution uniquely brings together core applications optimized for Risk mitigation and Collections on a single data-driven platform</p>
                 </div>
                 <div className='about-cards'>
                     <div className='about-card'>
                         <h3>Monitors risks & runs</h3>
                         <h3>diagnostics in near-real time</h3>
-                        <p>Simply execute this code in your JavaScript file or script, and it will remove all stored data in the local storage for the current domain. Simply execute this code in your JavaScript file or script, and it will remove .</p>
+                        <p>Our platform monitors risks and conducts near-real-time diagnostics, providing timely insights for informed decision-making in lending operations.</p>
                     </div>
                     <div className='about-card'>
-                        <h3>Monitors risks & runs</h3>
-                        <h3>diagnostics in near-real time</h3>
-                        <p>Simply execute this code in your JavaScript file or script, and it will remove all stored data in the local storage for the current domain. Simply execute this code in your JavaScript file or script, and it will remove .</p>
+                        <h3>Optimizes strategies and reduces time to deploy</h3>
+                        <p>Our platform optimizes strategies, reducing deployment time, and enabling agile decision-making to enhance performance and efficiency in lending operations.</p>
                     </div>
                     <div className='about-card'>
-                        <h3>Monitors risks & runs</h3>
-                        <h3>diagnostics in near-real time</h3>
-                        <p>Simply execute this code in your JavaScript file or script, and it will remove all stored data in the local storage for the current domain. Simply execute this code in your JavaScript file or script, and it will remove .</p>
-                    </div>
+                        <h3>Automates execution and monitors compliance</h3>
+                        <p>Our platform automates execution, ensuring efficient operations, while also monitoring compliance to industry standards, providing a comprehensive solution for lending institutions.</p>
+                        </div>
                 </div>
-                
             </div>
         </div>
     </>
